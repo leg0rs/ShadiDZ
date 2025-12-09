83eb8f13e4b74bf5184e6e1e995713bc0f84777d
leg0rs <legors1989@gmail.com>
2025-12-09 18:36:24 +0300

    feat: add Card

--- apps/frontend/src/app/(site)/page.tsx
+++ apps/frontend/src/app/(site)/page.tsx
@@ -1,28 +1,36 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { Button } from '@packages/ui/src/components/ui/button';
+import Card from '@/components/card';
 import { CountryResponseDto } from '@packages/utils/src/api/types.gen';
 import { getCountriesAction } from '../actions';
 const Page = () => {
 	const [countries, setCountries] = useState<CountryResponseDto[]>([]);
 
 	const GetCountries = async () => {
-		const countries = await getCountriesAction(1, 12);
+		const countries = await getCountriesAction(1, 48);
 		console.log(countries);
 		if (countries) {
 			setCountries(countries);
 		}
 	};
+
+	useEffect(() => {
+		GetCountries();
+	}, []);
 	return (
-		<div className="">
-			<Button onClick={GetCountries}>Get Countries</Button>
-			{countries.map((country) => (
-				<div key={country.cca2} className="text-2xl">
-					{country.cca2} - {country.name.common}
-				</div>
-			))}
+		<div className="ml-4 mr-4">
+			<input
+				type="text"
+				placeholder="Ввдеите"
+				className="flex justify-self-end h-10 bg-card text-card-foreground rounded-lg p-2 m-4"
+			/>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+				{countries.map((country) => (
+					<Card key={country.cca2} {...country}></Card>
+				))}
+			</div>
 		</div>
 	);
 };
--- apps/frontend/src/components/card.tsx
+++ apps/frontend/src/components/card.tsx
@@ -1,5 +1,35 @@
-const Card = () => {
-	return <div className=""></div>;
+import { CountryResponseDto } from '@packages/utils/src/api/types.gen';
+const Card = (props: CountryResponseDto) => {
+	return (
+		<div className="bg-card text-card-foreground p-4 rounded-lg m-3 shadow-custom-lg">
+			<header className="flex items-center justify-between">
+				<div className="text-3xl font-bold">{props.name.common}</div>
+				<div className="text-5xl">{props.flag}</div>
+			</header>
+			<main className="grid grid-cols-2 gap-4 mt-4 border-t-2 border-gray-200 pt-4">
+				<div className="min-w-0">
+					<div className="text-2xl font-bold">Столица</div>
+					<div className="text-2xl truncate" title={props.capital?.[0]}>
+						{props.capital?.[0] ?? '—'}
+					</div>
+				</div>
+				<div className="min-w-0 text-right">
+					<div className="text-2xl font-bold">Регион</div>
+					<div className="text-2xl truncate" title={props.region}>
+						{props.region ?? '—'}
+					</div>
+				</div>
+				<div className="min-w-0">
+					<div className="text-2xl font-bold">Население</div>
+					<div className="text-2xl truncate">{props.population?.toLocaleString() ?? '—'}</div>
+				</div>
+				<div className="min-w-0 text-right">
+					<div className="text-2xl font-bold">Площадь</div>
+					<div className="text-2xl truncate">{props.area?.toLocaleString() ?? '—'}</div>
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default Card;
